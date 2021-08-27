1ee6eb9f56a82d609239a9e3077ef7671355a9e7
Kazi Mamun <kazi299499@gmail.com>
2021-08-27 16:58:06 +0600

    second upload

--- backend/controllers/exerciseController.js
+++ backend/controllers/exerciseController.js
@@ -1,3 +1,23 @@
+const User = require('../model/userModel');
+const Exercise = require('../model/exerciseModel');
 module.exports = {
-    
+    allExercise(req, res){
+
+    },
+    addExercise(req, res){
+        let {email, description} = req.body;
+        const duration = Number(req.body.duration);
+        const date = Date.parse(req.body.date);
+        User.findOne({email})
+            .then(user=>{
+                if(!user){
+                    console.log("user not found")
+                } else {
+                    let userName = user.userName;
+                    let exercise = new Exercise({userName,description, duration, date});
+
+                }
+            })
+            .catch(err=>console.log(err))
+    }
 }
--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
-
+const {} = require('../controllers/exerciseController');
 
 module.exports= router;
